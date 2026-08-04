a2c460d22591976aa7d2a5b0dbc23a4297eccfd8
MahmoudTarek02 <123756533+MahmoudTarek02@users.noreply.github.com>
2026-08-05 02:02:19 +0300

    Signout

--- backend/src/modules/auth/auth.controller.ts
+++ backend/src/modules/auth/auth.controller.ts
@@ -83,6 +83,12 @@ class AuthController {
       });
     }
   }
+
+  async logout(req: Request, res: Response) {
+    return res.status(200).json({
+      message: "Sign out successful",
+    });
+  }
 }
 
 export default new AuthController();
--- backend/src/modules/auth/auth.routes.ts
+++ backend/src/modules/auth/auth.routes.ts
@@ -6,5 +6,6 @@ const router = Router();
 router.post("/register", authController.register);
 router.get("/verify-email", authController.verifyEmail);
 router.post("/login", authController.login);
+router.post("/logout", authController.logout);
 
 export default router;
